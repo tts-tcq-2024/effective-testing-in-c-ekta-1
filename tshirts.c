e42339c94b8e0ec9a716ed314741b2ff1cfcf497
ekta-1 <48455720+ekta-1@users.noreply.github.com>
2024-08-22 20:27:30 +0530

    Update tshirts.c

--- tshirts.c
+++ tshirts.c
@@ -2,7 +2,9 @@
 #include <assert.h>
 
 char size(int cms) {
-    if (cms < 38) {
+    if (cms < 0) {
+        return '\0'; 
+    } else if (cms < 38) {
         return 'S';
     } else if (cms <= 42) {
         return 'M';
@@ -12,15 +14,15 @@ char size(int cms) {
 }
 
 int main() {
+    assert(size(-1) == '\0'); 
+    assert(size(0) == '\0'); 
     assert(size(37) == 'S');
     assert(size(38) == 'M');
     assert(size(39) == 'M');
     assert(size(40) == 'M');
     assert(size(42) == 'M');
     assert(size(43) == 'L');
-    assert(size(-1) == 'S');
-    assert(size(0) == 'S');
 
-    printf("All tests passed successfully!\n");
+    printf("All is well (maybe!)\n");
     return 0;
 }
